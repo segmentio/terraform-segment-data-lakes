c4aa98b9ce5fef5765fcdf3b60256e9a5c4a37e9
Hitisha Damani <hitisha.damani@segment.com>
2021-11-05 16:30:26 -0500

    adding buildkite code

---
+++ .buildkite/terraform-lint-each.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+
+yq() {
+  docker run -i $(docker build -q - < .buildkite/Dockerfile.yq) "$@"
+}
+
+# for each project in atlantis.yaml, use the terraform docker image for the version
+# specified by that workspace, on that dir
+cat atlantis.yaml | \
+  yq -r '.projects[] | [.name, .dir, .terraform_version] | @tsv' | \
+  while IFS=$'\t' read -r name dir version; do
+    echo "--- lint workspace $name (dir: $dir) (version: $version)"
+    # -recursive added in 0.12
+    flags=""
+    case "$version" in
+      0.14.*)
+        flags="-recursive"
+        ;;
+      0.13.*)
+        flags="-recursive"
+        ;;
+      0.12.*)
+        flags="-recursive"
+        ;;
+      0.11.*)
+        ;;
+      *)
+        echo "don't know flags for version $version"
+        exit 1
+        ;;
+    esac
+    docker run -v `pwd`/"$dir":/workdir --workdir /workdir hashicorp/terraform:"$version" \
+      fmt $TERRAFORM_FLAGS $flags "." || {
+        echo '^^^ +++'
+        exit 1
+      }
+    echo '✔️'
+  done
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/segmentio/terraform-aws-data-lake
-
-go 1.17
