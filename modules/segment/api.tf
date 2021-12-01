c5637d293cbda90dd1c23d1540ef2ef9a62c96e7
Richard Gilmore <richard.gilmore@slicelife.com>
2021-12-01 17:21:29 +0000

    add cluster

--- modules/segment/api.tf
+++ modules/segment/api.tf
@@ -2,6 +2,6 @@
 
 resource "null_resource" "segment-setup" {
   provisioner "local-exec" {
-    command = "${path.module}/set_cluster.py -u ${var.url} -d ${var.cluster_id} -p ${var.token} -e ${var.environment}"
+    command = "python ${path.module}/set_cluster.py -u ${var.url} -d ${var.cluster_id} -p ${var.token} -e ${var.environment}"
   }
 }
