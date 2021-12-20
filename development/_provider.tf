0224e1f202695a680b00e552fa637a4f9fa24c72
Richard Gilmore <richard.gilmore@gmail.com>
2021-12-20 14:04:21 +0000

    adding tags for EMR repos

--- development/_provider.tf
+++ development/_provider.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region  = "us-east-1"
   profile = "development"
   version = "2.50"
+
+  default_tags {
+    tags = {
+      department = "data"
+      subteam    = "dataeng"
+      git = https://github.com/slicelife/terraform-aws-data-lake/
+      environment = development
+      terraformed = yes
+    }
+  }
 }
 
 terraform {
--- production/_provider.tf
+++ production/_provider.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region  = "us-east-1"
   profile = "production"
   version = "2.50"
+
+  default_tags {
+    tags = {
+      department = "data"
+      subteam    = "dataeng"
+      git = https://github.com/slicelife/terraform-aws-data-lake/
+      environment = production
+      terraformed = yes
+    }
+  }
 }
 
 terraform {
