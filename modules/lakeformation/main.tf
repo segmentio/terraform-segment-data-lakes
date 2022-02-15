be425e58f05abbbd45a90f29cfbde7232dc446d7
Hitisha Damani <hitisha.damani@segment.com>
2022-02-15 11:10:29 -0500

    adding IAMAllowedPrincipals group

--- modules/lakeformation/main.tf
+++ modules/lakeformation/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lakeformation_permissions" "segment_aws_lake_formation" {
-  principal   = "segment_emr_instance_profile_role"
-  permissions = ["SUPER"]
+  principal   = "IAMAllowedPrincipals"
+  permissions = ["ALL"]
 
   database {
     name       = var.name
