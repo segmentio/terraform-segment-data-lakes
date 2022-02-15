cad533ee24142e2661397d2f54bde28c14b8eebc
Hitisha Damani <hitisha.damani@segment.com>
2022-02-15 10:59:44 -0500

    changing role to principal

--- modules/lakeformation/main.tf
+++ modules/lakeformation/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lakeformation_permissions" "segment_aws_lake_formation" {
-  role        = "segment_emr_instance_profile_role"
+  principal   = "segment_emr_instance_profile_role"
   permissions = ["SUPER"]
 
   database {
