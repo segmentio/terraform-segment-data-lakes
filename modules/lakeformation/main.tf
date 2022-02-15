2c9d9e021bd4dcb8ebf7ef10125e5e0bf0d2400d
Hitisha Damani <hitisha.damani@segment.com>
2022-02-15 12:13:31 -0500

    adding arn

--- modules/lakeformation/main.tf
+++ modules/lakeformation/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lakeformation_permissions" "segment_aws_lake_formation" {
-  principal   = "IAMAllowedPrincipals"
+  principal   = "arn:aws:iam::874799288871:role/segment_emr_instance_profile_role"
   permissions = ["ALL"]
 
   database {
