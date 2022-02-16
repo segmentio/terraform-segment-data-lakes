b06b78eb9fe1a86698180935fc5e9b680596ef9f
Hitisha Damani <hitisha.damani@segment.com>
2022-02-16 09:42:03 -0500

    adding lakeformation code to glue

--- modules/lakeformation/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_lakeformation_permissions" "segment_aws_lake_formation" {
-  principal   = "arn:aws:iam::874799288871:role/segment_emr_instance_profile_role"
-  permissions = ["ALL"]
-
-  database {
-    name       = var.name
-  }
-
-}
