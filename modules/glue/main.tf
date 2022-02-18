b55c7b5140bf955605cf580f920388587e95f593
Hitisha Damani <hitisha.damani@segment.com>
2022-02-18 13:01:15 -0500

    adding the tf code for lake formation

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -3,6 +3,15 @@
 # The AWS Glue Data Catalog is the central schema registry in the AWS eco-system which 
 # makes the schema available to various tools like Athena, Spectrum, EMR etc.
 # https://www.terraform.io/docs/providers/aws/r/glue_catalog_database.html
+resource "aws_lakeformation_permissions" "segment_data_lake_formation" {
+  role        = var.principal
+  permissions = ["ALL"]
+
+  database {
+    name       = var.name
+  }
+}
+
 resource "aws_glue_catalog_database" "segment_data_lake_glue_catalog" {
   name        = "${var.name}"
   description = "${var.description}"
--- modules/glue/variables.tf
+++ modules/glue/variables.tf
@@ -1,3 +1,8 @@
+variable "principal" {
+  description = "The arn of the principal."
+  type        = "string"
+}
+
 variable "name" {
   description = "The name of the database."
   type        = "string"
