21c45d8be2621d53f096171506948762a955edf4
Hitisha Damani <hitisha.damani@segment.com>
2022-02-24 13:49:59 -0500

    removing changes in glue module

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -3,7 +3,6 @@
 # The AWS Glue Data Catalog is the central schema registry in the AWS eco-system which 
 # makes the schema available to various tools like Athena, Spectrum, EMR etc.
 # https://www.terraform.io/docs/providers/aws/r/glue_catalog_database.html
-
 resource "aws_glue_catalog_database" "segment_data_lake_glue_catalog" {
   name        = "${var.name}"
   description = "${var.description}"
