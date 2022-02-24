a95146e480fa7d22cf536f9e5c2d1234fbc442ff
Hitisha Damani <hitisha.damani@segment.com>
2022-02-24 12:53:32 -0500

    removing arn outputs

--- modules/iam/output.tf
+++ modules/iam/output.tf
@@ -10,10 +10,7 @@ output "iam_emr_autoscaling_role" {
   value = "${aws_iam_role.segment_emr_autoscaling_role.name}"
 }
 
-output "data_lake_iam_role_arn" {
-  value = "${aws_iam_role.segment_data_lake_iam_role.arn}"
+output "data_lake_iam_role" {
+  value = "${aws_iam_role.segment_data_lake_iam_role.name}"
 }
 
-output "iam_emr_instance_profile_arn" {
-  value = "${aws_iam_instance_profile.segment_emr_instance_profile.arn}"
-}
