3f0041819842f51df7c94f91f48c27965f6b6cdb
Hitisha Damani <hitisha.damani@segment.com>
2022-02-24 09:32:20 -0500

    adding output for iam arns

--- modules/lakeformation/main.tf
+++ modules/lakeformation/main.tf
@@ -1,12 +1,19 @@
-resource "aws_lakeformation_permissions" "segment_data_lake_formation" {
+resource "aws_lakeformation_permissions" "segment_data_lake_formation_table" {
+  principal = var.principal
+  permissions = ["ALL"]
+
+  table {
+    database_name = aws_lakeformation_permissions.segment_data_lake_formation_database.database.name
+    wildcard = true
+  }
+}
+
+resource "aws_lakeformation_permissions" "segment_data_lake_formation_database" {
   principal   = var.principal
   permissions = ["ALL"]
 
   database {
     name       = var.name
   }
-  table {
-    database_name = var.name
-    wildcard = true
-  }
+
 }
