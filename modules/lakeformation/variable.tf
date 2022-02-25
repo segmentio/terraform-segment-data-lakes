9aca861886786442d410c48596be2cb7c3a25bdb
Hitisha Damani <hitisha.damani@segment.com>
2022-02-25 14:48:30 -0500

    finalizing

--- modules/lakeformation/variable.tf
+++ modules/lakeformation/variable.tf
@@ -1,16 +1,6 @@
-/*variable "emr_instance_profile_role" {
-  description = "The arn of the emr role."
-  type        = "string"
-}
-
-variable "segment_datalake_role" {
-  description = "The arn of the segment datalake iam role."
-  type        = "string"
-}
-*/
 variable "name" {
   description = "The name of the database."
-  type        = list(string)
+  type        = set(string)
 }
 
 variable "iam_roles" {
