3c7c558be19e4d7024f3a75821ddfe858013e3d6
Hitisha Damani <hitisha.damani@segment.com>
2022-02-25 14:26:28 -0500

    trying lookup function

--- modules/lakeformation/main.tf
+++ modules/lakeformation/main.tf
@@ -1,5 +1,52 @@
-#Granting all permissions to the emr_instance_profile_role and segment_datalake_role.
-#These permissions are needed to run the sync on emr cluster and to manage the tables in the database.
+# Granting all permissions to the emr_instance_profile_role and segment_datalake_role.
+# These permissions are needed to run the sync on emr cluster and to manage the tables in the database.
+
+/*resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_table_permission_datalake_role" {
+  principal = segment_datalake_role
+  permissions = ["ALL"]
+
+  table {
+    database_name = var.name
+    wildcard = true
+  }
+}
+
+resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_database_permission_datalake_role" {
+  principal = segment_datalake_role
+  permissions = ["ALL"]
+
+  database {
+    name = var.name
+  }
+}
+
+resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_table_permission_emr_role" {
+  principal = emr_instance_profile_role
+  permissions = ["ALL"]
+
+  table {
+    database_name = var.name
+    wildcard = true
+  }
+}
+
+resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_database_permission_emr_role" {
+  principal = emr_instance_profile_role
+  permissions = ["ALL"]
+
+  database {
+    name = var.name
+  }
+}
+
+#This block will set the emr_instance_profile_role as a Database creator.
+resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_database_creator_permission_emr_role" {
+  principal   = emr_instance_profile_role
+  permissions = ["CREATE_DATABASE"]
+
+  catalog_resource = true
+}
+*/
 
 resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_table_permission" {
   for_each = var.iam_roles
@@ -26,9 +73,8 @@ resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_databas
 
 #This block will set the emr_instance_profile_role as a Database creator.
 resource "aws_lakeformation_permissions" "segment_datalake_lakeformation_database_creator_permission" {
-  principal   = tolist(var.iam_roles)[1]
+  principal   = lookup(var.iam_roles, emr_role)
   permissions = ["CREATE_DATABASE"]
 
   catalog_resource = true
 }
-
--- modules/lakeformation/variable.tf
+++ modules/lakeformation/variable.tf
@@ -1,3 +1,13 @@
+/*variable "emr_instance_profile_role" {
+  description = "The arn of the emr role."
+  type        = "string"
+}
+
+variable "segment_datalake_role" {
+  description = "The arn of the segment datalake iam role."
+  type        = "string"
+}
+*/
 variable "name" {
   description = "The name of the database."
   type        = "string"
@@ -5,5 +15,5 @@ variable "name" {
 
 variable "iam_roles" {
   description = "The arn of the segment datalake iam role."
-  type        = set(string)
+  type        = map(string)
 }
