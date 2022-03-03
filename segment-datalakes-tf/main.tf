bb09eaaae6d5308bb8d733ca7a80d4262923eaf8
Shayan Golafshani <shayan.golafshani@segment.com>
2022-03-03 11:22:41 -0700

    Updated syntax to v 0.12

--- segment-datalakes-tf/main.tf
+++ segment-datalakes-tf/main.tf
@@ -36,8 +36,8 @@ module "iam" {
   # names.
   suffix = "_shayan_golafshani"
 
-  s3_bucket    = "${aws_s3_bucket.segment_datalake_s3.id}"
-  external_ids = "${values(local.external_ids)}"
+  s3_bucket    = aws_s3_bucket.segment_datalake_s3.id
+  external_ids = values(local.external_ids)
 }
 
 # Creates an EMR Cluster that Segment uses for performing the final ETL on your
@@ -45,11 +45,11 @@ module "iam" {
 module "emr" {
   source = "git@github.com:segmentio/terraform-aws-data-lake//modules/emr?ref=v0.5.0"
 
-  s3_bucket = "${aws_s3_bucket.segment_datalake_s3.id}"
+  s3_bucket = aws_s3_bucket.segment_datalake_s3.id
   subnet_id = "subnet-00f137e4f3a6f8356" # Replace this with the subnet ID you want the EMR cluster to run in.
 
   # LEAVE THIS AS-IS
-  iam_emr_autoscaling_role = "${module.iam.iam_emr_autoscaling_role}"
-  iam_emr_service_role     = "${module.iam.iam_emr_service_role}"
-  iam_emr_instance_profile = "${module.iam.iam_emr_instance_profile}"
+  iam_emr_autoscaling_role = module.iam.iam_emr_autoscaling_role
+  iam_emr_service_role     = module.iam.iam_emr_service_role
+  iam_emr_instance_profile = module.iam.iam_emr_instance_profile
 }
