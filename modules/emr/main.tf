e20b302cc908e6742c98743dd33a6251aef0a404
myint2022 <myint@pomelofashion.com>
2022-07-05 15:20:39 +0700

    formatted

--- modules/emr/main.tf
+++ modules/emr/main.tf
@@ -106,7 +106,8 @@ EOF
     {
       "Classification": "spark-hive-site",
       "Properties": {
-        "hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"      }
+        "hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+	  }
     },
 	{
       "Classification": "yarn-site",
